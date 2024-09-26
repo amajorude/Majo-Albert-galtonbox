b52e174d683c6aca619d5813e7a5b53942ae3b04
Albertlovi <lopez.vidal.albert@gmail.com>
2024-09-26 17:12:01 +0200

    Change script

--- galtonbox.py
+++ galtonbox.py
@@ -1,28 +1,31 @@
-# IMPORTS
 import random 
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import norm
 import math
 
-N = 10000
-n = 15
+N = 100 # Number of balls
+n = 15 # Number of levels
 
-#  EXPERIMENT RESULTS
+
 board = [0 for x in range(n + 1)] 
 for i in range(N):
+    # For each ball simulate a Galton board
     x = 0
     for j in range(n):
        x += random.randint(0, 1)
     board[x] += 1
 
-# EXPERIMENT MEAN
+
+# CALCULATE MININGFULL PARAMETERS
+
+# Calcualte the mean
 exp_mean = 0
 for x in range(n+1):
     exp_mean += x*board[x]
 exp_mean = exp_mean/N
 
-# EXPERIMENT STANDARD DEVIATION
+# Calculate the standard deviation
 exp_std_dev = 0
 for x in range(n +1):
     exp_std_dev += board[x] * (x - exp_mean)**2
@@ -34,37 +37,35 @@ print('experiment standard deviation: ', exp_std_dev)
 # Scale the results
 scaled_board =[board[x]/N for x in range(n + 1)]
 
-# NORMAL DISTRIBUTION
 
+# NORMAL DISTRIBUTION
 mean = n/2
 std_dev = math.sqrt(n)/2
 
-print('gaussian mean: ', mean)
-print('gaussian standard deviation: ', std_dev)
+print('Gaussian mean: ', mean)
+print('Gaussian standard deviation: ', std_dev)
  
 x = np.linspace(0, n, 100)
 normal_dist = norm.pdf(x, mean, std_dev)
 
-# MEAN QUADRATIC ERROR
+# Calculate the mean quadratic error of the predictions with respect to the nornal function
 mean_quadratic_error = 0
 for i in range(n):
-    mean_quadratic_error = (scaled_board[i] - normal_dist[i])**2
-print('mean quadratic error: ', mean_quadratic_error)
+    mean_quadratic_error += (scaled_board[i] - normal_dist[i])**2
+mean_quadratic_error = math.sqrt(mean_quadratic_error)/N
+print('Mean quadratic error: ', mean_quadratic_error)
 
 
 # PLOT THE RESULTS
 
 # Plot the experiment results
-plt.bar(range(len(scaled_board)), scaled_board)
-plt.xlabel('Index')
-plt.ylabel('Value')
-plt.title('Bar Plot of Array') 
+plt.bar(range(len(scaled_board)), scaled_board, label='Binomial Results', color='#6495ED')
 
 # Plot the normal distribution
-plt.plot(x, normal_dist, color='red', label='Normal Distribution')
-plt.xlabel('Index')
-plt.ylabel('Value')
-plt.title('Bar Plot with Normal Distribution')
+plt.plot(x, normal_dist, color='black', label='Normal Distribution')
+plt.xlabel('k')
+plt.ylabel('p(X=k)')
+plt.title('Experiment Results vs Normal Distribution')
 
 plt.legend()
 plt.show()
